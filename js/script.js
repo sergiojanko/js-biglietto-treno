269334e81afaebfe1fb494e85b08f0d53823f6ce
sergiojanko <janko.sergio1998@gmail.com>
2021-07-01 15:52:16 +0200

    discount-calc

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ console.log(userAge);
 var kilometers = parseInt(prompt("Inserisci i chilometri da percorrere"));
 console.log(kilometers);
 
-var ticketrice;
+var discount = 1;
+
+if (userAge < 18) {
+    discount  =  20 / 100;
+} else if (userAge > 65) {
+    discount = 40 / 100;
+}
