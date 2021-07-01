2bda0a8af0aa8777e3fd09f6491fe6dc6dcdcd7f
sergiojanko <janko.sergio1998@gmail.com>
2021-07-01 16:07:40 +0200

    ticket-price-calc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
+// Collect user info
 var userAge = parseInt(prompt("Inserisci la tua èta"));
-console.log(userAge);
+console.log("userAge: ", userAge);
 
 var kilometers = parseInt(prompt("Inserisci i chilometri da percorrere"));
-console.log(kilometers);
+console.log("kilometers: ", kilometers);
 
 var discount = 1;
+var ticketPrice = kilometers * 0.21
 
+// Discount calc
 if (userAge < 18) {
     discount  =  20 / 100;
 } else if (userAge > 65) {
     discount = 40 / 100;
 }
+console.log("ticketPrice: ", ticketPrice)
+console.log("discount: ", discount)
+
+// Ticket price calc
+ticketPrice = ticketPrice - (ticketPrice * discount);
+console.log("ticketPrice: ", ticketPrice);
