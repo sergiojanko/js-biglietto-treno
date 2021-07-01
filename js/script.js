747d9c270a02f14e4f213cdb8bb0cb0e2f96f829
sergiojanko <janko.sergio1998@gmail.com>
2021-07-01 15:44:51 +0200

    collect-user-data

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var userAge = parseInt(prompt("Inserisci la tua èta"));
+console.log(userAge);
+
+var kilometers = parseInt(prompt("Inserisci i chilometri da percorrere"));
+console.log(kilometers);
+
+var ticketrice;
